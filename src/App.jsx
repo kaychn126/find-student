88b9684953d22fd604a785b48c38a243e91efc6f
kaychn126 <kaychn@126.com>
2023-09-03 17:18:53 +0800

    feat: add alias

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
@@ -10,48 +8,47 @@ function App() {
     setInputValue(e.target.value);
   };
   const studentList = [
-    "BEGMYRADOVAJANAN",
-    '波波',
-    '陈思远',
-    '储楚',
-    '段纯熙',
-    '何言涵',
-    '胡心泽',
-    '黄可心',
-    '姜彦宸',
-    '蒋惠然',
-    '赖振源',
-    '李妤涵',
-    '李豫喆',
-    '梁景程',
-    '刘婍',
-    '柳宝妮',
-    '潘思彤',
-    '石贝依',
-    '孙南希',
-    '孙诗涵',
-    '唐一程',
-    '童源清',
-    '王姜琪',
-    '王苏妍',
-    '王欣语',
-    '王一诺',
-    '王子楚',
-    '魏雨婷',
-    '翁弈杺',
-    '吴曼凝',
-    '吴莫然',
-    '徐艺航',
-    '徐意彤',
-    '宣毅枫',
-    '杨子醇',
-    '余子晴',
-    '袁毅晨',
-    '张城赫',
-    '张文悦',
-    '周俊言',
-    '周铄涵',
-    '朱浩铭',
+    { "name": "波波", "alias": "BEGMYRADOVAJANAN" },
+    { "name": "陈思远", "alias": "思远" },
+    { "name": "储楚", "alias": "储楚" },
+    { "name": "段纯熙", "alias": "纯熙" },
+    { "name": "何言涵", "alias": "言涵" },
+    { "name": "胡心泽", "alias": "心泽" },
+    { "name": "黄可心", "alias": "可心" },
+    { "name": "姜彦宸", "alias": "彦宸" },
+    { "name": "蒋惠然", "alias": "惠然" },
+    { "name": "赖振源", "alias": "振源" },
+    { "name": "李妤涵", "alias": "妤涵" },
+    { "name": "李豫喆", "alias": "豫喆" },
+    { "name": "梁景程", "alias": "景程" },
+    { "name": "刘婍", "alias": "刘婍" },
+    { "name": "柳宝妮", "alias": "宝妮" },
+    { "name": "潘思彤", "alias": "思彤" },
+    { "name": "石贝依", "alias": "贝依" },
+    { "name": "孙南希", "alias": "南希" },
+    { "name": "孙诗涵", "alias": "诗涵" },
+    { "name": "唐一程", "alias": "一程" },
+    { "name": "童源清", "alias": "源清" },
+    { "name": "王姜琪", "alias": "姜琪" },
+    { "name": "王苏妍", "alias": "苏妍" },
+    { "name": "王欣语", "alias": "欣语" },
+    { "name": "王一诺", "alias": "一诺" },
+    { "name": "王子楚", "alias": "子楚" },
+    { "name": "魏雨婷", "alias": "雨婷" },
+    { "name": "翁弈杺", "alias": "弈杺" },
+    { "name": "吴曼凝", "alias": "曼凝" },
+    { "name": "吴莫然", "alias": "莫然" },
+    { "name": "徐艺航", "alias": "艺航" },
+    { "name": "徐意彤", "alias": "意彤" },
+    { "name": "宣毅枫", "alias": "毅枫" },
+    { "name": "杨子醇", "alias": "子醇" },
+    { "name": "余子晴", "alias": "子晴" },
+    { "name": "袁毅晨", "alias": "毅晨" },
+    { "name": "张城赫", "alias": "城赫" },
+    { "name": "张文悦", "alias": "文悦" },
+    { "name": "周俊言", "alias": "俊言" },
+    { "name": "周铄涵", "alias": "铄涵" },
+    { "name": "朱浩铭", "alias": "浩铭" },
   ];
   const handleSubmit = () => {
     if (!inputValue || inputValue.length === 0) {
@@ -59,7 +56,7 @@ function App() {
     }
     const list = [];
     studentList.forEach((student) => {
-      if (inputValue.indexOf(student) < 0) {
+      if (inputValue.indexOf(student.name) < 0 && inputValue.indexOf(student.alias) < 0) {
         list.push(student);
       }
     });
@@ -70,7 +67,7 @@ function App() {
       <div style={{ alignItems: 'start' }}>
         <div
           style={{ width: '100%', textAlign: 'left', fontSize: 13 }}
-        >{`全部学生名单：${studentList}`}</div>
+        >{`全部学生名单：${studentList.map(student => student.name)}`}</div>
         <div style={{ height: '20vh', marginTop: 10 }}>
           <textarea
             type="text"
